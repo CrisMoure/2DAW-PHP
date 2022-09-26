9afbe34955ac4364908b5a879ee7ea8b85f0e474
CrisMoure <114055171+CrisMoure@users.noreply.github.com>
2022-09-26 12:04:15 +0200

    Update ej6b.php

--- ej6b.php
+++ ej6b.php
@@ -8,13 +8,14 @@ $num1=$num;
 $res = "";
 $fact = "1";
 
+    #con un bucle for vamos disminuyendo el num de 1 en 1
 for($num1; $num1 >1; $num1--){
-    $fact = $fact*$num1;
+    $fact = $fact*$num1;    #multiplicamos el factorial por el numero
 
-    $res .= ($num1)."x";
+    $res .= $num1."x";    #en una variable guardamos el numero que va a ir disminuyendo y la x para luego imprimir todo junto
 
 }
-
+#imprimimos
 echo "$num!=".$res."1=".$fact;
 
 ?>
