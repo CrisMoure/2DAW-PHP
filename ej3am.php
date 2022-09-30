89d02aa0239b75ebad85cb807ae7581986366faa
CrisMoure <114055171+CrisMoure@users.noreply.github.com>
2022-09-30 09:45:37 +0200

    Add files via upload

---
+++ ej3am.php
@@ -0,0 +1,28 @@
+<HTML>
+<HEAD><TITLE> EJ2AM – Arrays Multidimensionales</TITLE></HEAD>
+<BODY>
+<?php
+/*modificar el ejercicio anterior para mostrar la suma de los elementos por filas y por
+columnas. Los valores se almacenarán en dos arrays.crear una matriz de 3x5 mostrarla por pantalla imprimiendo los elementos por fila
+en primer lugar y a continuación por columna*/
+
+$matriz=array();    #creamos un array vacio
+
+for ($i=1; $i < 6; $i++){ #mediante un bucle for recorremos desde 1 hasta 5 para crear 5 columnas y asignar valores del 1 al 5
+    for ($j=1; $j < 4; $j++){ #mediante otro for recorremos de 1 a 4 para crear las columnas y asignar valores del 1 al 3
+        $matriz[$i][$j] = "($i,$j) = (elemento pos $i,$j) - "; #imprimimos
+        echo $matriz[$i][$j];  
+    }     
+}
+echo "<br><br>";
+for ($i=1; $i < 6; $i++){ 
+    for ($j=1; $j < 4; $j++){ 
+        $matriz[$j][$i] = "($j,$i) = (elemento pos $j,$i) - "; #para imprimirlo al reves le damos la vuelta a los valores
+        echo $matriz[$j][$i]; 
+    }     
+}
+
+
+?>
+</BODY>
+</HTML>
